34eb41e0390a384e72d4acdcec1f4e9702ecb36f
Tkmt-U <niseganr@yahoo.co.jp>
2020-12-25 00:57:32 +0900

    create appendix.md

---
+++ Calendar/appendix.md
@@ -0,0 +1,7 @@
+Timeクラスでも実装できる(やろうと思えば)。
+Time.now
+p Time.now.year
+p Time.now.strftime('%a')
+
+dates = (1..31).to_a
+dates = [*1..31]
--- Calendar/calendar.rb
+++ Calendar/calendar.rb
@@ -1,11 +1,3 @@
-# Timeクラスでも実装できる(やろうと思えば)。
-# Time.now
-# p Time.now.year
-# p Time.now.strftime('%a')
-
-# dates = (1..31).to_a
-# dates = [*1..31]
-
 require 'date'
 
 today = Date.today
